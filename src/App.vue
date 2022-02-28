87f3fba4840565a6814cab004d87595fc1241dbb
SuperLy <2472104875@qq.com>
2022-02-28 20:50:00 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Explain: xxx
+ * @Author: SuperLy
+ * @LastEditors: SuperLy
+ * @Date: 2022-02-28 15:30:37
+ * @LastEditTime: 2022-02-28 20:49:31
+ * @FilePath: \Lamb-UI\src\App.vue
+-->
+
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style lang="scss">
+</style>
